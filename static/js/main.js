67c15419493e30ec4229240d15b1e26df3a8bccb
Michael Rodler <michael.rodler@paluno.uni-due.de>
2022-01-24 13:29:58 +0100

    Show all deadlines of multi-deadline conferences at once.
    Breaking change: we **require** that every `deadline` field in
    `conferences.yml` is a real yaml list (e.g., `["2022-..."]` for single
    deadlines).
    
    We iterate over every conference and every deadline to create an entry
    for every conference/deadline pair. The deadline is identified by the
    index into the `deadline` list in the yaml.

--- static/js/main.js
+++ static/js/main.js
@@ -4,8 +4,9 @@ $(function() {
   deadlineByConf = {};
 
   {% for conf in site.data.conferences %}
-  {% assign conf_id = conf.name | append: conf.year | slugify %}
-  {% if conf.deadline == "TBA" %}
+  // {{ conf.name }} {{ conf.year }}
+  {% if conf.deadline[0] == "TBA" %}
+  {% assign conf_id = conf.name | append: conf.year | append: '-0' | slugify %}
   $('#{{ conf_id }} .timer').html("TBA");
   $('#{{ conf_id }} .deadline-time').html("TBA");
   deadlineByConf["{{ conf_id }}"] = null;
@@ -46,37 +47,41 @@ $(function() {
       parsedDeadlines.push(deadline);
     }
   }
+  // due to pop before; we need to reverse such that the i index later matches
+  // the right parsed deadline
+  parsedDeadlines.reverse();
 
-  // check which deadline is closest
-  var confDeadline = parsedDeadlines[0];
-  var today = moment();
-  for (var i = 1; i < parsedDeadlines.length; i++) {
-    deadlineCandidate = parsedDeadlines[i];
-    if ((today.diff(deadlineCandidate) < 0 && today.diff(deadlineCandidate) > today.diff(confDeadline))) {
-      confDeadline = deadlineCandidate;
-    }
-  }
+  {% assign range_end = conf.deadline.size | minus: 1 %}
+  {% for i in (0..range_end) %}
+  {% assign conf_id = conf.name | append: conf.year | append: '-' | append: i | slugify %}
+  var deadlineId = {{ i }};
+  if (deadlineId < parsedDeadlines.length) {
+    var confDeadline = parsedDeadlines[deadlineId];
 
-  // render countdown timer
-  if (confDeadline) {
-    function make_update_countdown_fn(confDeadline) {
-      return function(event) {
-        diff = moment() - confDeadline
-        if (diff <= 0) {
-           $(this).html(event.strftime('%D days %Hh %Mm %Ss'));
-        } else {
-          $(this).html(confDeadline.fromNow());
+    // render countdown timer
+    if (confDeadline) {
+      function make_update_countdown_fn(confDeadline) {
+        return function(event) {
+          diff = moment() - confDeadline
+          if (diff <= 0) {
+             $(this).html(event.strftime('%D days %Hh %Mm %Ss'));
+          } else {
+            $(this).html(confDeadline.fromNow());
+          }
         }
       }
+      $('#{{ conf_id }} .timer').countdown(confDeadline.toDate(), make_update_countdown_fn(confDeadline));
+      // check if date has passed, add 'past' class to it
+      if (moment() - confDeadline > 0) {
+        $('#{{ conf_id }}').addClass('past');
+      }
+      $('#{{ conf_id }} .deadline-time').html(confDeadline.local().format('D MMM YYYY, h:mm:ss a'));
+      deadlineByConf["{{ conf_id }}"] = confDeadline;
     }
-    $('#{{ conf_id }} .timer').countdown(confDeadline.toDate(), make_update_countdown_fn(confDeadline));
-    // check if date has passed, add 'past' class to it
-    if (moment() - confDeadline > 0) {
-      $('#{{ conf_id }}').addClass('past');
-    }
-    $('#{{ conf_id }} .deadline-time').html(confDeadline.local().format('D MMM YYYY, h:mm:ss a'));
-    deadlineByConf["{{ conf_id }}"] = confDeadline;
+  } else {
+    // TODO: hide the conf_id ?
   }
+  {% endfor %}
   {% endif %}
   {% endfor %}
 
