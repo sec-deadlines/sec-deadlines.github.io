b3bcf368b53db647c69cdce581aeccd5b8a9f2ad
Hocheol Nam <nhc616@gmail.com>
2026-04-13 14:09:48 +0900

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,11 @@
 ---
 ---
 $(function() {
-  deadlineByConf = {};
+  var deadlineByConf = {};
 
   {% for conf in site.data.conferences %}
   // {{ conf.name }} {{ conf.year }}
   {% if conf.deadline[0] == "TBA" %}
-  //{% assign conf_id = conf.name | append: conf.year | append: '-0' | slugify %}
   {% assign conf_type = conf.tags | join: "-" | slugify %}
   {% assign conf_id = conf.name | append: conf.year | append: '-0' | append: conf_type | slugify %}
   $('#{{ conf_id }} .timer').html("TBA");
@@ -18,12 +17,15 @@ $(function() {
   if (rawDeadlines.constructor !== Array) {
     rawDeadlines = [rawDeadlines];
   }
+
   var parsedDeadlines = [];
   while (rawDeadlines.length > 0) {
     var rawDeadline = rawDeadlines.pop();
+
     // deal with year template in deadline
-    year = {{ conf.year }};
+    var year = {{ conf.year }};
     rawDeadline = rawDeadline.replace('%y', year).replace('%Y', year - 1);
+
     // adjust date according to deadline timezone
     {% if conf.timezone %}
     var deadline = moment.tz(rawDeadline, "YYYY-M-D HH:mm", "{{ conf.timezone }}");
@@ -40,8 +42,7 @@ $(function() {
     }
     parsedDeadlines.push(deadline);
   }
-  // due to pop before; we need to reverse such that the i index later matches
-  // the right parsed deadline
+
   parsedDeadlines.reverse();
 
   {% assign range_end = conf.deadline.size | minus: 1 %}
@@ -51,28 +52,32 @@ $(function() {
   if (deadlineId < parsedDeadlines.length) {
     var confDeadline = parsedDeadlines[deadlineId];
 
-    // render countdown timer
     if (confDeadline) {
       function make_update_countdown_fn(confDeadline) {
         return function(event) {
-          diff = moment() - confDeadline
+          var diff = moment() - confDeadline;
           if (diff <= 0) {
-             $(this).html(event.strftime('%D days %Hh %Mm %Ss'));
+            $(this).html(event.strftime('%D days %Hh %Mm %Ss'));
           } else {
             $(this).html(confDeadline.fromNow());
           }
-        }
+        };
       }
-      $('#{{ conf_id }} .timer').countdown(confDeadline.toDate(), make_update_countdown_fn(confDeadline));
-      // check if date has passed, add 'past' class to it
+
+      $('#{{ conf_id }} .timer').countdown(
+        confDeadline.toDate(),
+        make_update_countdown_fn(confDeadline)
+      );
+
       if (moment() - confDeadline > 0) {
         $('#{{ conf_id }}').addClass('past');
       }
-      $('#{{ conf_id }} .deadline-time').html(confDeadline.local().format('D MMM YYYY, h:mm:ss a'));
+
+      $('#{{ conf_id }} .deadline-time').html(
+        confDeadline.local().format('D MMM YYYY, h:mm:ss a')
+      );
       deadlineByConf["{{ conf_id }}"] = confDeadline;
     }
-  } else {
-    // TODO: hide the conf_id ?
   }
   {% endfor %}
   {% endif %}
@@ -81,11 +86,14 @@ $(function() {
   // Reorder list
   var today = moment();
   var confs = $('.conf').detach();
+
   confs.sort(function(a, b) {
     var aDeadline = deadlineByConf[a.id];
     var bDeadline = deadlineByConf[b.id];
-    var aDiff = today.diff(aDeadline);
-    var bDiff = today.diff(bDeadline);
+
+    var aDiff = aDeadline ? today.diff(aDeadline) : Number.POSITIVE_INFINITY;
+    var bDiff = bDeadline ? today.diff(bDeadline) : Number.POSITIVE_INFINITY;
+
     if (aDiff < 0 && bDiff > 0) {
       return -1;
     }
@@ -94,22 +102,21 @@ $(function() {
     }
     return bDiff - aDiff;
   });
+
   $('.conf-container').append(confs);
 
-  // Set checkboxes
   // Read filter data from Jekyll
   var filter1 = {{ site.data.filters.filter1 | jsonify }};
   var filter2 = {{ site.data.filters.filter2 | jsonify }};
   var filter3 = {{ site.data.filters.filter3 | jsonify }};
 
-  // Combine all filters into a single array
   var all_tags = [];
   var toggle_status = {};
 
   function processFilters(filters) {
     for (var i = 0; i < filters.length; i++) {
-      all_tags.push(filters[i]['tag']);
-      toggle_status[filters[i]['tag']] = false;
+      all_tags.push(filters[i].tag);
+      toggle_status[filters[i].tag] = false;
     }
   }
 
@@ -117,53 +124,67 @@ $(function() {
   processFilters(filter2);
   processFilters(filter3);
 
-  // Retrieve stored preferences
-  var tags = store.get('{{ site.domain }}');
-  if (tags === undefined) {
-    tags = []; // Default to all unchecked
-  }
-
-  // Apply stored preferences to checkboxes
-  for (var i = 0; i < all_tags.length; i++) {
-      var tag = all_tags[i];
-      var isChecked = tags.includes(tag); // Check if the tag is stored
-      $('#' + tag + '-checkbox').prop('checked', isChecked);
-      toggle_status[tag] = isChecked;
-  }
-
-  // Save updated selection to local storage
-  store.set('{{ site.domain }}', tags);
-
-   
-   
   // Track selected filters
-  let selectedFilters = {
+  var selectedFilters = {
     filter1: new Set(),
     filter2: new Set(),
     filter3: new Set()
   };
 
-  function updateConfList() {
-    $(".conf").each(function () {
-      let conf = $(this);
-      let show = true;
+  // Retrieve stored preferences
+  var tags = store.get('{{ site.domain }}');
+  if (!Array.isArray(tags)) {
+    tags = [];
+  }
 
-      // Check each filter group
-      Object.keys(selectedFilters).forEach(filterGroup => {
+  // Apply stored preferences to checkboxes AND selectedFilters
+  for (var i = 0; i < all_tags.length; i++) {
+    var tag = all_tags[i];
+    var isChecked = tags.includes(tag);
+    var $checkbox = $('#' + tag + '-checkbox');
+
+    $checkbox.prop('checked', isChecked);
+    toggle_status[tag] = isChecked;
+
+    if (isChecked) {
+      var filterGroup = $checkbox.data('filter-group');
+      if (filterGroup && selectedFilters[filterGroup]) {
+        selectedFilters[filterGroup].add(tag);
+      }
+    }
+  }
+
+  function saveSelectedTags() {
+    var selectedTags = [];
+    $('.filter-checkbox:checked').each(function() {
+      selectedTags.push($(this).attr('id').replace('-checkbox', ''));
+    });
+    store.set('{{ site.domain }}', selectedTags);
+  }
+
+  function updateConfList() {
+    $('.conf').each(function() {
+      var conf = $(this);
+      var show = true;
+
+      Object.keys(selectedFilters).forEach(function(filterGroup) {
+        if (!show) return;
+
         if (selectedFilters[filterGroup].size > 0) {
-          let hasTag = false;
-          selectedFilters[filterGroup].forEach(tag => {
+          var hasTag = false;
+
+          selectedFilters[filterGroup].forEach(function(tag) {
             if (conf.hasClass(tag)) {
               hasTag = true;
             }
           });
+
           if (!hasTag) {
             show = false;
           }
         }
       });
 
-      // Show or hide based on filter matching
       if (show) {
         conf.show();
       } else {
@@ -173,18 +194,19 @@ $(function() {
   }
 
   // Handle checkbox changes
-  $(".filter-checkbox").change(function () {
-    let tag = $(this).attr("id").replace("-checkbox", "");
-    let filterGroup = $(this).data("filter-group");
+  $('.filter-checkbox').change(function() {
+    var tag = $(this).attr('id').replace('-checkbox', '');
+    var filterGroup = $(this).data('filter-group');
 
-    if ($(this).is(":checked")) {
+    if ($(this).is(':checked')) {
       selectedFilters[filterGroup].add(tag);
     } else {
       selectedFilters[filterGroup].delete(tag);
     }
 
+    saveSelectedTags();
     updateConfList();
   });
 
-  updateConfList(); // Initial display
+  updateConfList();
 });
